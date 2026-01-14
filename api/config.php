d57d6d61a3017f2926219e0736dd95437f316002
ryoshken <ryoshken@gmail.com>
2026-01-14 17:48:55 +0800

    Switch project structure for Standard PHP Hosting (InfinityFree)

---
+++ api/config.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+// Configure your database settings here
+define('DB_HOST', 'sqlxxx.infinityfree.com'); // Update this
+define('DB_PORT', 3306);
+define('DB_NAME', 'if0_38293938_migewlito'); // Update this
+define('DB_USER', 'if0_38293938'); // Update this
+define('DB_PASS', 'your_password'); // Update this
